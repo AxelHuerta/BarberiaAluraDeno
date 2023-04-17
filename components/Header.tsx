f45427d72983c45e74d1d3698c788fec3ebfb19e
Axel Huerta <axelhuerta.dev@gmail.com>
2023-04-16 20:03:54 -0600

    add Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
+
 export function Header() {
   return (
-    <header style={{backgroundColor: '#7b98a4'}}>
-      Lorem
+    <header style={{backgroundColor: '#7b98a4'}} class='flex justify-around uppercase font-bold items-center'>
+      <img src="https://raw.githubusercontent.com/AxelHuerta/BarberiaAluraDeno/main/assets/images/logo.png" alt="Logo" />
+      <nav>
+        <ul class="flex text-2xl justify-between w-full">
+          <li class="mx-5">home</li>
+          <li class="mx-5">productos</li>
+          <li class="mx-5">contacto</li>
+        </ul>
+      </nav>
     </header>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,6 @@
 import { Head } from "$fresh/runtime.ts";
 import { Header } from "../components/Header.tsx";
-import Counter from "../islands/Counter.tsx";
+import { Banner } from "../components/Banner.tsx";
 
 export default function Home() {
   return (
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
       <>
       <Header />
+      <Banner />
       </> 
     </>
   );
